fb4cf3d410e6797c671582d0fd8fbea5cc879496
Nolo <26887899+nolosb@users.noreply.github.com>
2021-11-01 21:15:58 +0000

    Remove user preference setting

--- javascripts/discourse/connectors/discovery-list-container-top/featured-tiles.js
+++
@@ -1,8 +0,0 @@
-export default {
-  shouldRender() {
-    let val = window.localStorage.getItem("show_featured_topics_banner");
-    if (val === "false") val = false;
-    if (val === null) val = true;
-    return val;
-  }
-};
--- javascripts/discourse/connectors/user-preferences-interface/featured-tiles.js
+++
@@ -1,7 +0,0 @@
-import User from "discourse/models/user";
-
-export default {
-  shouldRender(component) {
-    return component.model.id === User.current().id;
-  }
-};
