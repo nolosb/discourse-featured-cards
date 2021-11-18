2f8babc9ddcc97b383e1da862fe98dd09c4ca336
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-11-18 16:25:54 +0530

    DEV: fixes more than 2 tags issue

--- javascripts/discourse/components/featured-cards.js
+++ javascripts/discourse/components/featured-cards.js
@@ -9,7 +9,7 @@ const displayCategories = settings.display_categories
   .map((id) => parseInt(id, 10))
   .filter((id) => id);
 
-const featuredTags = settings.featured_tags.replace("|", " ");
+const featuredTags = encodeURI(settings.featured_tags.replace("|", " "));
 
 function shuffle(array) {
   array = [...array];
