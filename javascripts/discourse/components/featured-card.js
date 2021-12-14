d7919d5605e111234500a2457cf822b3b3b58822
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-12-14 13:39:22 +0530

    DEV: fixes tag url

--- javascripts/discourse/components/featured-card.js
+++ javascripts/discourse/components/featured-card.js
@@ -9,7 +9,7 @@ export default Component.extend({
 
   @discourseComputed("topic.tags")
   tagUrl(tags) {
-    return `/tags/${this.tag}`;
+    return `/tag/${this.tag}`;
   },
 
   @discourseComputed("topic.tags")
